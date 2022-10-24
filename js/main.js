f3a76ee78408906a8e1f594df6492dad8fcd1b2b
Luis Treviño <97487146+LuisTrevio@users.noreply.github.com>
2022-10-24 02:39:52 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,4 +6,6 @@ function ClickMenu() {
     document.querySelectorAll('.br-3').forEach((result) => {result.classList.toggle('bar3-on');})
 
         document.querySelectorAll('.menu-on').forEach((result) => {result.classList.toggle('Menu-Full-on');})
+
+        document.querySelectorAll('.scr-fr').forEach((result) => {result.classList.toggle('scroll-frost');})
 }
